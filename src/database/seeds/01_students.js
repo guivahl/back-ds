1845f533551647e3ecf7f90b9102eda446425313
Leandro-Weber <leandrowebertavares@gmail.com>
2023-04-12 20:48:36 -0300

    added new seeds to populate db

--- src/database/seeds/01_students.js
+++ src/database/seeds/01_students.js
@@ -2,6 +2,11 @@ exports.seed = async (knex) => {
   const alunos = [
     { userEmail: 'gdsvahl@inf.ufpel.edu.br', registrationNumber: '19100784' },
     { userEmail: 'marcelo.sdias@inf.ufpel.edu.br', registrationNumber: '19100793' },
+    { userEmail: 'lwtavares@inf.ufpel.edu.br', registrationNumber: '19100824' },
+    { userEmail: 'argalvao@inf.ufpel.edu.br', registrationNumber: '19100359' },
+    { userEmail: 'mamota@inf.ufpel.edu.br', registrationNumber: '19101316' },
+    { userEmail: 'ldsguths@inf.ufpel.edu.br', registrationNumber: '19100392' },
+
   ];
 
   await knex('students').del();
