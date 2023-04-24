02b440e11e60410e1e107266bf6cd167efe4a906
leandrowtavares <leandrowebertavares@gmail.com>
2023-04-24 16:06:00 -0300

    refact: deleted duplicate require from professorRoutes

--- src/routes/professorsRoutes.js
+++ src/routes/professorsRoutes.js
@@ -3,8 +3,6 @@ const { Router } = require('express');
 const ProfessorController = require('../controllers/ProfessorController');
 const Authentication = require('../middlewares/Authentication');
 
-const Authentication = require('../middlewares/Authentication');
-
 const professorRouter = Router();
 
 professorRouter.get('/professors', Authentication.checkIfUserIsProfessor, ProfessorController.index);
