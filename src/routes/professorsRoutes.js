cc0e334aceb6cd6afd431f489f36c9a1a48d2b96
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2023-04-24 12:16:18 -0300

    fix: add auth to professores routes

--- src/routes/professorsRoutes.js
+++ src/routes/professorsRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 
 const ProfessorController = require('../controllers/ProfessorController');
+const Authentication = require('../middlewares/Authentication');
 
 const professorRouter = Router();
 
-professorRouter.get('/professors', ProfessorController.index);
-professorRouter.get('/professors/classes', ProfessorController.getAllClasses);
+professorRouter.get('/professors', Authentication.checkIfUserIsProfessor, ProfessorController.index);
+professorRouter.get('/professors/classes', Authentication.checkIfUserIsProfessor, ProfessorController.getAllClasses);
 
 module.exports = professorRouter;
