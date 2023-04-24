a40a4d5ab53b49df48f30a87e533bf7696cb4233
leandrowtavares <leandrowebertavares@gmail.com>
2023-04-24 15:51:17 -0300

    fix: added authentication for professors/reviews

--- src/routes/professorsRoutes.js
+++ src/routes/professorsRoutes.js
@@ -2,9 +2,11 @@ const { Router } = require('express');
 
 const ProfessorController = require('../controllers/ProfessorController');
 
+const Authentication = require('../middlewares/Authentication');
+
 const professorRouter = Router();
 
 professorRouter.get('/professors', ProfessorController.index);
-professorRouter.get('/professors/reviews', ProfessorController.getProposalsToReview);
+professorRouter.get('/professors/reviews', Authentication.checkIfUserIsProfessor, ProfessorController.getProposalsToReview);
 
 module.exports = professorRouter;
