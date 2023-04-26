7b8c6710d54d89a6c73958410b0c39c94cf83e3b
Leandro-Weber <leandrowebertavares@gmail.com>
2023-04-25 21:13:53 -0300

    refactored seeds with more entries

--- src/database/seeds/00_users.js
+++ src/database/seeds/00_users.js
@@ -2,16 +2,22 @@ const HashService = require('../../services/HashService');
 
 exports.seed = async (knex) => {
   const users = [
+    { name: 'Administrador', email: 'adm@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
     { name: 'Guilherme dos Santos Vahl', email: 'gdsvahl@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
     { name: 'Marcelo Dias', email: 'marcelo.sdias@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
-    { name: 'Cristiano Flores dos Santos', email: 'cfdsantos@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
     { name: 'Leandro Weber Tavares', email: 'lwtavares@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
-    { name: 'Larissa Freitas', email: 'larissa@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
-    { name: 'Administrador', email: 'adm@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
     { name: 'Alessandra Rosa Galvão', email: 'argalvao@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
     { name: 'Murillo Aleixo Mota', email: 'mamota@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
     { name: 'Leonardo dos Santos Guths', email: 'ldsguths@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
+    { name: 'Bruno da Silva Volcan', email: 'bdsvolcan@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
     { name: 'André du Bois', email: 'adubois@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
+    { name: 'Larissa Freitas', email: 'larissa@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
+    { name: 'Cristiano Flores dos Santos', email: 'cfdsantos@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
+    { name: 'Rafael Piccin Torchelsen', email: 'rafael.torchelsen@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
+    { name: 'Marilton Sanchotene Aguiar', email: 'marilton@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
+    { name: 'Guilherme Ribeiro Corrêa', email: 'gcorrea@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
+    { name: 'Renata Hax Sander Reiser', email: 'reiser@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
+    { name: 'Ulisses Brisolara Corrêa', email: 'ulisses@inf.ufpel.edu.br', password: HashService.generateHash('senha123') },
 
   ];
 
