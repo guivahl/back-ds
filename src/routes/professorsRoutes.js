67130030c2e7b6e4764dd0fcfb889b01fa4f3bb2
Leandro-Weber <leandrowebertavares@gmail.com>
2023-04-26 22:14:17 -0300

    refactored: removed authentication from /professors

--- src/routes/professorsRoutes.js
+++ src/routes/professorsRoutes.js
@@ -5,7 +5,7 @@ const Authentication = require('../middlewares/Authentication');
 
 const professorRouter = Router();
 
-professorRouter.get('/professors', Authentication.checkIfUserIsProfessor, ProfessorController.index);
+professorRouter.get('/professors', ProfessorController.index);
 professorRouter.get('/professors/reviews', Authentication.checkIfUserIsProfessor, ProfessorController.getProposalsToReview);
 professorRouter.get('/professors/classes', Authentication.checkIfUserIsProfessor, ProfessorController.getAllClasses);
 
