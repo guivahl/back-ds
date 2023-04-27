780b0cdbab124ca61e94cc2616d4fb6adfa880cb
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2023-04-26 21:00:19 -0300

    refactor: change filename on multer

--- src/routes/proposalsRoutes.js
+++ src/routes/proposalsRoutes.js
@@ -6,7 +6,15 @@ const ProposalController = require('../controllers/ProposalController');
 
 const proposalsRouter = Router();
 
-const upload = multer({ dest: 'uploads/' });
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, 'uploads/');
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname);
+    }
+});
+const upload = multer({ storage });
 
 proposalsRouter.post('/proposals', upload.single('file'), ProposalController.create);
 
