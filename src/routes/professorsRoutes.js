2b50e743298a473e34eab0897631892551aadf65
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2023-05-08 13:08:59 -0300

    refact: professors endpoint to fetch proposals by classes

--- src/routes/professorsRoutes.js
+++ src/routes/professorsRoutes.js
@@ -6,7 +6,7 @@ const Authentication = require('../middlewares/Authentication');
 const professorRouter = Router();
 
 professorRouter.get('/professors', ProfessorController.index);
-professorRouter.get('/professors/reviews', Authentication.checkIfUserIsProfessor, ProfessorController.getProposalsToReview);
+professorRouter.get('/professors/classes/:id/proposals', Authentication.checkIfUserIsProfessor, ProfessorController.getProposalsToReview);
 professorRouter.get('/professors/classes', Authentication.checkIfUserIsProfessor, ProfessorController.getAllClasses);
 
 module.exports = professorRouter;
