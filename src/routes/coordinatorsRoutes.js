4a4ef5e45672be58c038b20b1f5334cf6e70cf37
marcelosdias <marcelo.sdias@inf.ufpel.edu.br>
2023-05-09 14:40:33 -0300

    refact: adding plural to coordinatorsRoutes

--- src/routes/coordinatorsRoutes.js
+++ src/routes/coordinatorsRoutes.js
@@ -6,6 +6,6 @@ const CoordinatorMiddleware = require('../middlewares/CoordinatorMiddleware');
 
 const coordinatorRouter = Router();
 
-coordinatorRouter.post('/coordinator/proposals/:id/reviewers', CoordinatorMiddleware.checkLinkReviewerToProposal, CoodinatorController.linkProfessorsToProposal);
+coordinatorRouter.post('/coordinators/proposals/:id/reviewers', CoordinatorMiddleware.checkLinkReviewerToProposal, CoodinatorController.linkProfessorsToProposal);
 
 module.exports = coordinatorRouter;
