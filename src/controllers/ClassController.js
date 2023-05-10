722f46af2f4226141eec7eff46829bc7813f991c
Leandro-Weber <leandrowebertavares@gmail.com>
2023-05-10 16:42:20 -0300

    feat: edit class by id

--- src/controllers/ClassController.js
+++ src/controllers/ClassController.js
@@ -58,6 +58,28 @@ class ClassController {
 
     return response.status(200).json({ status: 'Turma adicionada com sucesso.' });
   }
+
+  async update(request, response) {
+    const classInfo = request.body;
+    const dates = classInfo.dates.split(';');
+
+    await Class.query().upsertGraph({
+      id: classInfo.classId,
+      name: classInfo.name,
+      startDate: dates[0],
+      endDate: dates[1],
+      submissionStartDate: dates[2],
+      submissionEndDate: dates[3],
+      evaluationStartDate: dates[4],
+      evaluationEndDate: dates[5],
+      resubmissionStartDate: dates[6],
+      resubmissionEndDate: dates[7],
+      reevaluationStartDate: dates[8],
+      reevaluationEndDate: dates[9],
+    });
+
+    return response.status(200).json({ status: 'Truma modificada com sucesso.' });
+  }
 }
 
 module.exports = new ClassController();
