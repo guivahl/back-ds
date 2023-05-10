4cd6f8513948fca4b71b4669c7e2ab355c3e39a0
Leandro-Weber <leandrowebertavares@gmail.com>
2023-05-10 13:54:19 -0300

    feat: crud for professors

--- src/controllers/ProfessorController.js
+++ src/controllers/ProfessorController.js
@@ -1,6 +1,9 @@
 const Proposal = require('../models/Proposal');
 const User = require('../models/User');
 const Class = require('../models/Class');
+const Professor = require('../models/Professor');
+const Review = require('../models/Review');
+const HashService = require('../services/HashService');
 
 class ProfessorController {
   async index(request, response) {
@@ -24,9 +27,9 @@ class ProfessorController {
       .where('proposals.classId', turmaId)
       .orderBy([
         { column: 'reviews.wasApproved', order: 'desc' },
-        { column: 'proposals.createdAt', order: 'desc' }, 
+        { column: 'proposals.createdAt', order: 'desc' },
       ]);
-      
+
     return response.json(proposals);
   }
 
@@ -35,18 +38,18 @@ class ProfessorController {
     const { id: turmaId } = request.params;
 
     const proposals = await Proposal.query()
-      .withGraphJoined("[class(filterClass), reviews.reviewer.user]", { joinOperation: 'innerJoin'})
+      .withGraphJoined('[class(filterClass), reviews.reviewer.user]', { joinOperation: 'innerJoin' })
       .modifiers({
         filterClass: (builder) => {
-          builder.where("classes.coordinatorEmail", email);
+          builder.where('classes.coordinatorEmail', email);
         },
       })
-      .where("proposals.classId", turmaId)
+      .where('proposals.classId', turmaId)
       .orderBy([
-        { column: "reviews.wasApproved", order: "desc" },
-        { column: "proposals.createdAt", order: "desc" },
+        { column: 'reviews.wasApproved', order: 'desc' },
+        { column: 'proposals.createdAt', order: 'desc' },
       ]);
-      
+
     return response.json(proposals);
   }
 
@@ -81,6 +84,84 @@ class ProfessorController {
       advisorClasses: advisor,
       reviewerClasses: reviewer,
     });
+  }
+
+  async create(request, response) {
+    const professor = request.body;
+    const verifyProfessor = await User
+      .query()
+      .findById(professor.email);
+
+    if (verifyProfessor) {
+      return response.status(400).json({ status: 'Usuário com este email já cadastrado.' });
+    }
+    const trx = await User.startTransaction();
+
+    await User
+      .query(trx)
+      .insert({
+        email: professor.email,
+        name: professor.name,
+        password: HashService.generateHash(professor.password),
+      });
+    const insertProfessor = await Professor
+      .query(trx)
+      .insert({ userEmail: professor.email, employeeNumber: professor.employeeNumber });
+
+    await trx.commit();
+
+    if (!insertProfessor) { return response.status(500).json({ status: 'Erro ao adicionar professor.' }); }
+
+    return response.status(200).json({ status: 'Professor adicionado com sucesso.' });
+  }
+
+  async update(request, response) {
+    const professor = request.body;
+    await User.query().upsertGraph({
+      email: professor.email,
+      name: professor.name,
+      professor: {
+        employeeNumber: professor.employeeNumber,
+      },
+    });
+
+    return response.status(200).json({ status: 'Professor modificado com sucesso.' });
+  }
+
+  async delete(request, response) {
+    const professor = request.body;
+
+    const professorExists = await User.query()
+      .where('email', professor.email);
+
+    if (professorExists.length === 0) {
+      return response.status(400).json({ status: 'Professor não encontrado.' });
+    }
+    const professorProposals = await Proposal.query()
+      .where('advisorEmail', professor.email);
+
+    if (professorProposals.length > 0) {
+      return response.status(400).json({ status: 'Professor já possui vínculo uma ou mais propostas no sistema, não é possível deletar.' });
+    }
+
+    const professorReviews = await Review.query()
+      .where('reviewerEmail', professor.email);
+
+    if (professorReviews.length > 0) {
+      return response.status(400).json({ status: 'Professor já possui vínculo uma ou mais revisões no sistema, não é possível deletar.' });
+    }
+
+    const professorClasses = await Class.query()
+      .where('coordinatorEmail', professor.email);
+
+    if (professorClasses.length > 0) {
+      return response.status(400).json({ status: 'Professor já possui vínculo uma ou mais turmas no sistema, não é possível deletar.' });
+    }
+
+    await User.query()
+      .deleteById(professor.email);
+
+    return response.status(200).json({ status: 'Professor deletado com sucesso.' });
   }
 }
 
--- src/routes/professorsRoutes.js
+++ src/routes/professorsRoutes.js
@@ -9,5 +9,8 @@ professorRouter.get('/professors', ProfessorController.index);
 professorRouter.get('/professors/classes/:id/proposals/reviews', Authentication.checkIfUserIsProfessor, ProfessorController.getProposalsToReview);
 professorRouter.get('/professors/classes/:id/proposals/coordinations', Authentication.checkIfUserIsProfessor, ProfessorController.getProposalsToCoordinate);
 professorRouter.get('/professors/classes', Authentication.checkIfUserIsProfessor, ProfessorController.getAllClasses);
+professorRouter.post('/professors', Authentication.checkIfUserIsAdmin, ProfessorController.create);
+professorRouter.put('/professors', Authentication.checkIfUserIsAdmin, ProfessorController.update);
+professorRouter.delete('/professors', Authentication.checkIfUserIsAdmin, ProfessorController.delete);
 
 module.exports = professorRouter;
